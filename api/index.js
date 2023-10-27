901245355fe4360c41d159bfee5ffcd75180051c
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2023-10-28 02:41:04 +0530

    connected sever with mongoDB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MongoUrl)
+.then(() => {
+    console.log("Database connected successfully!");
+})
+.catch((err) => {
+    console.log(err.message);
+})
 
 app.listen(3000, () => {
     console.log('Port is running on port 3000!');
 })
+
 
