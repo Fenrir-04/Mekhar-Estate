5423f1631c14eee21d28ccb0b81a2f6b1c2f9fef
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2023-10-30 21:55:29 +0530

    created sign-up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.router.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -19,6 +20,9 @@ app.listen(3000, () => {
     console.log('Port is running on port 3000!'); //make sever listen on the port 3000
 });
 
-app.use('/api/user', userRouter);
+app.use(express.json());
+
+app.use('/api/user', userRouter); // test api route
+app.use('/api/auth', authRouter); // signup api route
 
 
