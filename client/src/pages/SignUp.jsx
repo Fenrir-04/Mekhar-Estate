2d2469e9adb99b4b816fd35e39a1d8c91a817600
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2023-10-31 02:57:28 +0530

    created sign up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-3'>
+        <input type='text' placeholder='username' className='p-3 rounded-lg border' id='username' />
+        <input type='text' placeholder='email' className='p-3 rounded-lg border' id='email' />
+        <input type='text' placeholder='password' className='p-3 rounded-lg border' id='password' />
+        <button className='bg-red-700 text-white rounded-lg p-3 hover:opacity-90 font-medium uppercase disabled:opacity-80'>Sign up</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Already have an account?</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-700'>sign in</span>
+        </Link>
+      </div>
+    </div>
   )
 }
