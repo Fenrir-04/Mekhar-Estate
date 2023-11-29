7fc68a7b8876dcd5caa73fb7484ec3e7048d9ef8
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2023-11-30 03:08:12 +0530

    completed profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+
+  const { currentUser} = useSelector((state) => state.user);
+
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold my-7 text-center'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img src={currentUser.avatar} alt='profile'
+        className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+        <input type='text' placeholder='Username'
+        className='border p-3 rounded-lg' id='username' />
+        <input type='email' placeholder='Email'
+        className='border p-3 rounded-lg' id='email' />
+        <input type='text' placeholder='Password'
+        className='border p-3 rounded-lg' id='password' />
+        <button className='rounded-lg p-3 bg-blue-700 text-white uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-500 bg-black rounded-xl p-1 cursor-pointer hover:opacity-95 disabled:opacity-80'>Delete Account</span>
+        <span className='text-red-500 bg-black rounded-xl p-1 cursor-pointer hover:opacity-95 disabled:opacity-80'>Sign Out</span>
+      </div>
+    </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   
   server: {
-    host: "localhost",
     port: 3000,
     proxy: {
       '/api': {
