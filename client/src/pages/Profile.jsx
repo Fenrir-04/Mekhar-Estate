ae3fa74c06a528bea346f4e82c204aebc3406c3f
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2023-11-30 16:08:14 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,27 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useRef } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase.js';
 
 export default function Profile() {
 
   const { currentUser} = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined); // for the pics, at start it's empty
+  const [filePerc, setFilePerc] = useState(0); // to display the pefcentage of image uploaded on screen, inigtially it's 0
+  const [fileUploadError, setFileUploadError] = useState(false); // to get the error while file uploading
+  const [formData, setFormData] = useState({}); // to upload the data in profile section
+
+  // console.log(formData); 
+  // console.log(filePerc);
+  // console.log(file);
+  // console.log(fileUploadError);
+
+  useEffect(() => {
+    if(file){
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app); // the same app to start the firebase server in the firebase.js
+    const fileName = new Date().getTime() + file.name; // date is added to get the unique name and avoid same name for two files
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', 
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setFilePerc(Math.round(progress));
+    },
+
+    (error) => {
+      setFileUploadError(true);
+    },
+
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref)
+      .then((downloadURL) => {
+        setFormData({...formData, avatar: downloadURL});
+      });
+    }
+    );
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold my-7 text-center'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt='profile'
-        className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+        <input onChange={(e) => setFile(e.target.files[0])} // set the recieved image as the new image in setFile
+         type='file' 
+         ref={fileRef} // helps to keep the image same and avoid re-render of site
+         hidden // hide it in screeen
+         accept='image/*' // accepts only image type file
+        />
+
+        <img onClick={() => fileRef.current.click()}
+         src={formData.avatar || currentUser.avatar}
+          alt='profile'
+        className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+
+        <p className='text-sm self-center'>
+          {
+            fileUploadError ? 
+            (<span className='text-red-600'>Error Image Upload</span>) :
+              (filePerc > 0 && filePerc < 100) ?
+              (<span className='text-slate-700'>{`uploading ${filePerc}%`}</span>) :
+              (filePerc === 100) ?
+                (<span className='text-green-600'>Image uploaded successfully!</span>)
+                : ""
+            
+
+          }
+        </p>
+
         <input type='text' placeholder='Username'
         className='border p-3 rounded-lg' id='username' />
+
         <input type='email' placeholder='Email'
         className='border p-3 rounded-lg' id='email' />
+
         <input type='text' placeholder='Password'
         className='border p-3 rounded-lg' id='password' />
+
         <button className='rounded-lg p-3 bg-blue-700 text-white uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-500 bg-black rounded-xl p-1 cursor-pointer hover:opacity-95 disabled:opacity-80'>Delete Account</span>
-        <span className='text-red-500 bg-black rounded-xl p-1 cursor-pointer hover:opacity-95 disabled:opacity-80'>Sign Out</span>
+        <span className='text-white bg-red-600 rounded-xl p-1 cursor-pointer hover:opacity-95 disabled:opacity-80'>Delete Account</span>
+        <span className='text-white bg-red-600 rounded-xl p-1 cursor-pointer hover:opacity-95 disabled:opacity-80'>Sign Out</span>
       </div>
     </div>
   )
