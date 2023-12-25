3eff5003e5fa1e0ee361439fac36653b2c22fc70
Fenrir09 <102972384+Fenrir09@users.noreply.github.com>
2023-12-25 15:53:00 +0530

    created get user listing Api route

--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {deleteUser, test, updateUser, } from '../controllers/user.controller.js';
+import {deleteUser, test, updateUser, getUserListings} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,7 +8,7 @@ router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser); // first func will verify if the user's data is valid,
 //if yes, then proceed to update the profile else it will fail to go to next function
 router.delete('/delete/:id', verifyToken, deleteUser);
-
+router.get('/listings/:id', verifyToken, getUserListings);
 
 
 export default router;
